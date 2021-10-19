b1313382a3a6132301d81e9c976410c34a367e40
Joseph Adams <11955644+adamsjoe@users.noreply.github.com>
2021-10-19 21:36:29 +0100

    Update csv_reader.py

--- py_data/csv_reader.py
+++ py_data/csv_reader.py
@@ -1,29 +1,29 @@
 import csv
 import sys
 import pprint
+import os
 from google.cloud import bigquery
 pp = pprint.PrettyPrinter(indent=4)
-import os
-import pandas as pd
 
 # constants
 SERVICE_KEY_jSON = './service_key.json'
-FILENAME_IN = '../yearCSV/bq-results-2013.csv'
-#FILENAME_IN = '/content/drive/MyDrive/bq-results-20211014-193236-1h75k989rhl5/bq-results-20211014-193236-1h75k989rhl5.csv'
-FILENAME_OUT = '../Final_data_2013.csv'
+FILENAME_IN = 'bq-results-2017-jan.csv'
+FILENAME_OUT = 'Final_data_2017-jan.csv'
 
 # set OS environment
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=SERVICE_KEY_jSON
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = SERVICE_KEY_jSON
+
 
 def getBoroghFromLatLong(lat, long):
-    print(f'Entered Borough Query')
+    print('Entered Borough Query')
     returnVal = ""
     client = bigquery.Client()         # Start the BigQuery Client
-    QUERY = ('SELECT UPPER(borough) AS BOROUGH FROM `bigquery-public-data.new_york_taxi_trips.taxi_zone_geom` tz_loc WHERE (ST_DWithin(tz_loc.zone_geom, ST_GeogPoint('+ str(long) +', '+ str(lat) +'),0))')
-    
+    QUERY = (
+        'SELECT UPPER(borough) AS BOROUGH FROM `bigquery-public-data.new_york_taxi_trips.taxi_zone_geom` tz_loc WHERE (ST_DWithin(tz_loc.zone_geom, ST_GeogPoint(' + str(long) + ', ' + str(lat) + '),0))')
+
     query_job = client.query(QUERY)    # Start Query API Request
-    query_result = query_job.result()  # Get Query Result 
-    df = query_result.to_dataframe() 
+    query_result = query_job.result()  # Get Query Result
+    df = query_result.to_dataframe()
 
     try:
         returnVal = str(df["BOROUGH"].values[0])
@@ -31,6 +31,7 @@ def getBoroghFromLatLong(lat, long):
         returnVal = "Unknown"
 
     return returnVal
+
 
 def open_file(file_in, skip_header=True):
     print(f'Opening file {file_in}')
@@ -57,6 +58,7 @@ def open_file(file_in, skip_header=True):
     # return the data variable (the file contents)
     return data
 
+
 def create_report(file_name, headers, content):
     try:
         with open(file_name, 'a', newline="") as out_file:
@@ -70,6 +72,7 @@ def create_report(file_name, headers, content):
         )
         sys.exit(1)
 
+
 def collate_data(data_in):
     x = 1
     unknown_rows = 0
@@ -79,10 +82,11 @@ def collate_data(data_in):
     print(f'Processing {linesInFile} lines')
 
     output_dict = {}
-    
-    for row in data_in:   
-        # make a little progress counter - otherwise we have no clue what's happening
-        pcent = round((x / linesInFile * 100),2)
+
+    for row in data_in:
+        # make a little progress counter -
+        # otherwise we have no clue what's happening
+        pcent = round((x / linesInFile * 100), 2)
         print(f'Processing row - {x} - {pcent}%')
 
         # make things easier to reference
@@ -106,13 +110,15 @@ def collate_data(data_in):
         prcp = row[17]
         sndp = row[18]
         fog = row[19]
-        
-        # someone forgot to fill in numbers, so we need to check something is here as there are nulls in the data
+
+        # someone forgot to fill in numbers, so we
+        # need to check something is here as there
+        # are nulls in the data
         if row[20] == "":
             cyclists_killed = 0
         else:
             cyclists_killed = int(row[20])
-        
+
         if row[21] == "":
             cyclists_injured = int(row[21])
         else:
@@ -120,29 +126,29 @@ def collate_data(data_in):
 
         if row[22] == "":
             motorists_killed = 0
-        else:            
+        else:
             motorists_killed = int(row[22])
-        
+
         if row[23] == "":
             motorists_injured = 0
         else:
             motorists_injured = int(row[23])
-        
+
         if row[24] == "":
             peds_killed = 0
         else:
             peds_killed = int(row[24])
-        
+
         if row[25] == "":
             peds_injured = 0
         else:
             peds_injured = int(row[25])
 
         if row[26] == "":
-                persons_killed = 0
+            persons_killed = 0
         else:
             persons_killed = int(row[26])
-        
+
         if row[27] == "":
             persons_injured = 0
         else:
@@ -159,47 +165,49 @@ def collate_data(data_in):
         # deal with the horrid "BB" - these are entries which have no latitude, longitude or borough.
         if neighborhood == "BB":
             neighborhood = getBoroghFromLatLong(lat, long)
-            print(f'     --> Handling missing borogh - from {lat} and {long} got {neighborhood}')
+            print(
+                f'     --> Handling missing borogh - from {lat} and {long} got {neighborhood}')
             found_rows += 1
         else:
             neighborhood = neighborhood
-    
-        # we have bad data we cannot do anything with, I've marked this as neighbourhood "Unknown" - skip over this
+
+        # we have bad data we cannot do anything with, I've marked this as
+        # neighbourhood "Unknown" - skip over this
         if neighborhood == "Unknown":
-            print(f'     Skipping row - unusable data')
+            print('     Skipping row - unusable data')
             unknown_rows += 1
             continue
-        
+
         if neighborhood not in output_dict[collision_date]:
-            output_dict[collision_date][neighborhood]  = {}
-            output_dict[collision_date][neighborhood]["day"]  = ""
-            output_dict[collision_date][neighborhood]["year"]  = ""
-            output_dict[collision_date][neighborhood]["mo"]  = ""
-            output_dict[collision_date][neighborhood]["da"]  = ""
+            output_dict[collision_date][neighborhood] = {}
+            output_dict[collision_date][neighborhood]["day"] = ""
+            output_dict[collision_date][neighborhood]["year"] = ""
+            output_dict[collision_date][neighborhood]["mo"] = ""
+            output_dict[collision_date][neighborhood]["da"] = ""
 
-            output_dict[collision_date][neighborhood]["temp"]  = 0
-            output_dict[collision_date][neighborhood]["dewp"]  = 0
-            output_dict[collision_date][neighborhood]["slp"]  = 0
-            output_dict[collision_date][neighborhood]["visib"]  = 0
-            output_dict[collision_date][neighborhood]["wdsp"]  = 0
-            output_dict[collision_date][neighborhood]["mxpsd"]  = 0
-            output_dict[collision_date][neighborhood]["gust"]  = 0
-            output_dict[collision_date][neighborhood]["max"]  = 0
-            output_dict[collision_date][neighborhood]["min"]  = 0
-            output_dict[collision_date][neighborhood]["prcp"]  = 0
-            output_dict[collision_date][neighborhood]["sndp"]  = 0
-            output_dict[collision_date][neighborhood]["fog"]  = 0
+            output_dict[collision_date][neighborhood]["temp"] = 0
+            output_dict[collision_date][neighborhood]["dewp"] = 0
+            output_dict[collision_date][neighborhood]["slp"] = 0
+            output_dict[collision_date][neighborhood]["visib"] = 0
+            output_dict[collision_date][neighborhood]["wdsp"] = 0
+            output_dict[collision_date][neighborhood]["mxpsd"] = 0
+            output_dict[collision_date][neighborhood]["gust"] = 0
+            output_dict[collision_date][neighborhood]["max"] = 0
+            output_dict[collision_date][neighborhood]["min"] = 0
+            output_dict[collision_date][neighborhood]["prcp"] = 0
+            output_dict[collision_date][neighborhood]["sndp"] = 0
+            output_dict[collision_date][neighborhood]["fog"] = 0
 
-            output_dict[collision_date][neighborhood]["cyclists_killed"]  = 0
-            output_dict[collision_date][neighborhood]["cyclists_injured"]  = 0
-            output_dict[collision_date][neighborhood]["motorists_killed"]  = 0
-            output_dict[collision_date][neighborhood]["motorists_injured"]  = 0
-            output_dict[collision_date][neighborhood]["peds_killed"]  = 0
-            output_dict[collision_date][neighborhood]["peds_injured"]  = 0
-            output_dict[collision_date][neighborhood]["persons_killed"]  = 0
-            output_dict[collision_date][neighborhood]["persons_injured"]  = 0
-            output_dict[collision_date][neighborhood]["num_collisions"]  = 0
-        
+            output_dict[collision_date][neighborhood]["cyclists_killed"] = 0
+            output_dict[collision_date][neighborhood]["cyclists_injured"] = 0
+            output_dict[collision_date][neighborhood]["motorists_killed"] = 0
+            output_dict[collision_date][neighborhood]["motorists_injured"] = 0
+            output_dict[collision_date][neighborhood]["peds_killed"] = 0
+            output_dict[collision_date][neighborhood]["peds_injured"] = 0
+            output_dict[collision_date][neighborhood]["persons_killed"] = 0
+            output_dict[collision_date][neighborhood]["persons_injured"] = 0
+            output_dict[collision_date][neighborhood]["num_collisions"] = 0
+
         # get the ccurrent things we want to deal with
         current_temp = temp
         current_dewp = dewp
@@ -239,30 +247,30 @@ def collate_data(data_in):
         output_dict[collision_date][neighborhood]["year"] = year
         output_dict[collision_date][neighborhood]["mo"] = mo
         output_dict[collision_date][neighborhood]["da"] = da
-        output_dict[collision_date][neighborhood]["temp"]  = current_temp
-        output_dict[collision_date][neighborhood]["dewp"]  = current_dewp
-        output_dict[collision_date][neighborhood]["slp"]  = current_slp
-        output_dict[collision_date][neighborhood]["visib"]  = current_visib
-        output_dict[collision_date][neighborhood]["wdsp"]  = current_wdsp
-        output_dict[collision_date][neighborhood]["mxpsd"]  = current_mxpsd
-        output_dict[collision_date][neighborhood]["gust"]  = current_gust
-        output_dict[collision_date][neighborhood]["max"]  = current_max
-        output_dict[collision_date][neighborhood]["min"]  = current_min
-        output_dict[collision_date][neighborhood]["prcp"]  = current_prcp
-        output_dict[collision_date][neighborhood]["sndp"]  = current_sndp
-        output_dict[collision_date][neighborhood]["fog"]  = current_fog
-        output_dict[collision_date][neighborhood]["cyclists_killed"]  = update_cyclists_killed
-        output_dict[collision_date][neighborhood]["cyclists_injured"]  = update_cyclists_injured
-        output_dict[collision_date][neighborhood]["motorists_killed"]  = update_motorists_killed
-        output_dict[collision_date][neighborhood]["motorists_injured"]  = update_motorists_injured
-        output_dict[collision_date][neighborhood]["peds_killed"]  = update_peds_killed
-        output_dict[collision_date][neighborhood]["peds_injured"]  = update_peds_injured
-        output_dict[collision_date][neighborhood]["persons_killed"]  = update_persons_killed
-        output_dict[collision_date][neighborhood]["persons_injured"]  = update_persons_injured
-        output_dict[collision_date][neighborhood]["num_collisions"]  = update_num_collisions
+        output_dict[collision_date][neighborhood]["temp"] = current_temp
+        output_dict[collision_date][neighborhood]["dewp"] = current_dewp
+        output_dict[collision_date][neighborhood]["slp"] = current_slp
+        output_dict[collision_date][neighborhood]["visib"] = current_visib
+        output_dict[collision_date][neighborhood]["wdsp"] = current_wdsp
+        output_dict[collision_date][neighborhood]["mxpsd"] = current_mxpsd
+        output_dict[collision_date][neighborhood]["gust"] = current_gust
+        output_dict[collision_date][neighborhood]["max"] = current_max
+        output_dict[collision_date][neighborhood]["min"] = current_min
+        output_dict[collision_date][neighborhood]["prcp"] = current_prcp
+        output_dict[collision_date][neighborhood]["sndp"] = current_sndp
+        output_dict[collision_date][neighborhood]["fog"] = current_fog
+        output_dict[collision_date][neighborhood]["cyclists_killed"] = update_cyclists_killed
+        output_dict[collision_date][neighborhood]["cyclists_injured"] = update_cyclists_injured
+        output_dict[collision_date][neighborhood]["motorists_killed"] = update_motorists_killed
+        output_dict[collision_date][neighborhood]["motorists_injured"] = update_motorists_injured
+        output_dict[collision_date][neighborhood]["peds_killed"] = update_peds_killed
+        output_dict[collision_date][neighborhood]["peds_injured"] = update_peds_injured
+        output_dict[collision_date][neighborhood]["persons_killed"] = update_persons_killed
+        output_dict[collision_date][neighborhood]["persons_injured"] = update_persons_injured
+        output_dict[collision_date][neighborhood]["num_collisions"] = update_num_collisions
         x += 1
 
-    print(f'Found {found_rows} rows, but had to drop {unknown_rows} rows')                
+    print(f'Found {found_rows} rows, but had to drop {unknown_rows} rows')
     return output_dict
 
 
@@ -271,41 +279,44 @@ data = open_file(FILENAME_IN)
 contents = collate_data(data)
 
 # headers for use in the CSV output
-headers = ['DATE', 'BOROUGH', 'WEEKDAY', 'YEAR', 'MONTH', 'DAY', 'COLLISION_DATE', 'TEMP', 'DEWP', 'SLP', 'VISIB', 'WDSP', 'MXPSD', 'GUST', 'MAX', 'MIN', 'PRCP', 'SNDP', 'FOG', 'CYC_KILL', 'CYC_INJD', 'MOTO_KILL', 'MOTO_INJD', 'PEDS_KILL','PEDS_INJD', 'PERS_KILL', 'PERS_INJD','NUM_COLS']
+headers = ['DATE', 'BOROUGH', 'WEEKDAY', 'YEAR', 'MONTH', 'DAY', 'COLLISION_DATE', 'TEMP', 'DEWP', 'SLP', 'VISIB', 'WDSP', 'MXPSD', 'GUST', 'MAX', 'MIN', 'PRCP', 'SNDP', 'FOG', 'CYC_KILL', 'CYC_INJD', 'MOTO_KILL', 'MOTO_INJD', 'PEDS_KILL', 'PEDS_INJD', 'PERS_KILL', 'PERS_INJD','NUM_COLS']
 
 # output list
 output = []
 
 # print the headers on screen (formatted) so we know things have happened
 print(
-    "{:<15}  {:<15}     {:<8}      {:<8}   {:<8}    {:<8}  {:<15}            {:<8}   {:<8}   {:<8}  {:<8}    {:<8}   {:<8}    {:<8}   {:<8}  {:<8}  {:<8}   {:<8}   {:<8}  {:<10}      {:<10}      {:<15}       {:<10}       {:<10}      {:10}        {:<10}       {:<10}     {:<10}".format
-    ('DATE', 'BOROUGH', 'WEEKDAY', 'YEAR', 'MONTH', 'DAY', 'COLLISION_DATE', 'TEMP', 'DEWP', 'SLP', 'VISIB', 'WDSP', 'MXPSD', 'GUST', 'MAX', 'MIN', 'PRCP', 'SNDP', 'FOG', 'CYC_KILL', 'CYC_INJD', 'MOTO_KILL', 'MOTO_INJD', 'PEDS_KILL','PEDS_INJD', 'PERS_KILL', 'PERS_INJD','NUM_COLS')
+    "{:<15} {:<15} {:<8} {:<8} {:<8} {:<8} {:<15} {:<8} {:<8}   {:<8}  {:<8}    {:<8}   {:<8}    {:<8}   {:<8}  {:<8}  {:<8}   {:<8}   {:<8}  {:<10}      {:<10}      {:<15}       {:<10}       {:<10}      {:10}        {:<10}       {:<10}     {:<10}".format
+    ('DATE', 'BOROUGH', 'WEEKDAY', 'YEAR', 'MONTH', 'DAY', 'COLLISION_DATE',
+     'TEMP', 'DEWP', 'SLP', 'VISIB', 'WDSP', 'MXPSD', 'GUST', 'MAX', 'MIN',
+     'PRCP', 'SNDP', 'FOG', 'CYC_KILL', 'CYC_INJD', 'MOTO_KILL', 'MOTO_INJD',
+     'PEDS_KILL', 'PEDS_INJD', 'PERS_KILL', 'PERS_INJD', 'NUM_COLS')
     )
 
 for key in contents:
     row_date = ""
     subkeys = ""
-    
+
     # print(key) #gives me dates
     row_date = key
 
     # print(len(contents[key]))
 
     for subkeys in contents[key]:
-        
+
         # print(subkeys)
         # print(row_date + "  " + subkeys)
         dayVal = contents[key][subkeys].get("day")
-        
+
         yearVal = contents[key][subkeys].get("year")
         monthVal = contents[key][subkeys].get("mo")
         daVal = contents[key][subkeys].get("da")
-        
+
         coll_date = row_date
 
         tempVal = contents[key][subkeys].get("temp")
         dewpVal	= contents[key][subkeys].get("dewp")
-        slpVal = contents[key][subkeys].get("slp")	
+        slpVal = contents[key][subkeys].get("slp")
         visibVal = contents[key][subkeys].get("visib")
         wdspVal = contents[key][subkeys].get("wdsp")
         mxpsdVal = contents[key][subkeys].get("mxpsd")
@@ -330,7 +341,6 @@ for key in contents:
 
         num_collisionsVa = contents[key][subkeys].get("num_collisions")
 
-
         item = row_date, subkeys, dayVal, yearVal, monthVal, daVal, coll_date, tempVal, dewpVal, slpVal, visibVal, wdspVal, mxpsdVal, gustVal, maxVal, minVal, prcpVal, sndpVal, fogVal, cyclists_killedVal, cyclists_injuredVal, motorists_killedVal, motorists_injuredVal, peds_killedVal, peds_injuredVal, peds_killedVal, persons_injuredVal, num_collisionsVa
 
         output.append(item)
@@ -341,4 +351,3 @@ for key in contents:
             )
 
 create_report(FILENAME_OUT, headers, output)
-    
